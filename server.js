adb954b7ae02291c76250563312759ee8f333d25
Reginald Tan <redge.tan@gmail.com>
2023-10-06 23:08:20 -0400

    bump @google-cloud/speech

--- server.js
+++ server.js
@@ -51,13 +51,91 @@ mediaws.on('connect', function(connection) {
   new MediaStreamHandler(connection);
 });
 
+const REPEAT_THRESHOLD = 50;
+
 class MediaStreamHandler {
   constructor(connection) {
     this.metaData = null;
     this.trackHandlers = {};
     this.connection = connection
+    this.messages = []
+    this.repeatCount = 0
     connection.on('message', this.processMessage.bind(this));
     connection.on('close', this.close.bind(this));
+  }
+
+  repeat() {
+    const messages = [...this.messages];
+    this.messages = [];
+    const streamSid = messages[0].streamSid;
+
+    // Decode each message and store the bytes in an array
+    const messageByteBuffers = messages.map((msg) =>
+      Buffer.from(msg.media.payload, "base64")
+    );
+    // Combine all the bytes, and then base64 encode the entire payload.
+    const payload = Buffer.concat(messageByteBuffers).toString("base64");
+    const message = {
+      event: "media",
+      streamSid,
+      media: {
+        payload,
+      },
+    };
+    const messageJSON = JSON.stringify(message);
+    const payloadRE = /"payload":"[^"]*"/gi;
+    log(
+      `To Twilio: A single media event containing the exact audio from your previous ${messages.length} inbound media messages`,
+      messageJSON.replace(
+        payloadRE,
+        `"payload":"an omitted base64 encoded string with length of ${message.media.payload.length} characters"`
+      )
+    );
+    this.connection.sendUTF(messageJSON);
+
+    // Send a mark message
+    const markMessage = {
+      event: "mark",
+      streamSid,
+      mark: {
+        name: `Repeat message ${this.repeatCount}`,
+      },
+    };
+    log("To Twilio: Sending mark event", markMessage);
+    this.connection.sendUTF(JSON.stringify(markMessage));
+    this.repeatCount++;
+    if (this.repeatCount === 5) {
+      log(`Server: Repeated ${this.repeatCount} times...closing`);
+      this.connection.close(1000, "Repeated 5 times");
+    }
+  }
+
+  replyWithEcho(message) {
+    const messages = [...this.messages] // do copy instead of reference
+    this.messages = []
+    const messageByteBuffers = messages.map((msg) =>
+      Buffer.from(msg.media.payload, "base64")
+    );
+    // Combine all the bytes, and then base64 encode the entire payload.
+    const payload = Buffer.concat(messageByteBuffers).toString("base64");
+    this.connection.sendUTF(JSON.stringify({
+      streamSid,
+      event: "media",
+      media: {
+        payload: payload
+      }
+    }))
+
+    // send mark
+    const markMessage = {
+      event: "mark",
+      streamSid,
+      mark: {
+        name: `echo message`,
+      },
+    };
+    log("To Twilio: Sending mark event", markMessage);
+    this.connection.sendUTF(JSON.stringify(markMessage));
   }
 
   processMessage(message){
@@ -72,20 +150,17 @@ class MediaStreamHandler {
         return;
       }
       const track = data.media.track;
+
+      if (data.event === 'media') {
+        this.messages.push(data);
+      }
+
       if (this.trackHandlers[track] === undefined) {
         const service = new TranscriptionService();
         service.on('transcription', (transcription) => {
           log(`Transcription (${track}): ${transcription}`);
-
-          // send echo
-          this.connection.sendUTF(JSON.stringify({
-            streamSid,
-            event: "media",
-            media: {
-              track: 'outbound',
-              payload: data.media.payload
-            }
-          }))
+          
+          this.replyWithEcho()
         })
 
         this.trackHandlers[track] = service;
